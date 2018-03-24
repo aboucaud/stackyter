0831578baa6b16acc132d9ef6680ea3bab11f2a8
Alexandre Boucaud <alexandre.boucaud@ias.u-psud.fr>
2018-03-24 02:03:07 +0100

    Enforce line length < 79 characters

--- stackyter.py
+++ stackyter.py
@@ -24,7 +24,8 @@ def get_default_config(only_path=False):
     if os.getenv("STACKYTERCONFIG") is not None:  # set up by the user
         config = os.getenv("STACKYTERCONFIG")
         if not os.path.exist(config):
-            raise IOError("$STACKYTERCONFIG is defined but the file does not exist.")
+            raise IOError("$STACKYTERCONFIG is defined but the file does "
+                          "not exist.")
     elif os.path.exists(DEFAULT_CONFIG):  # default location
         config = DEFAULT_CONFIG
     else:
@@ -38,16 +39,23 @@ def read_config(config, key=None):
     config = yaml.load(open(config, 'r'))
     if key is not None:
         if key in config:
-            print("INFO: Using the '%s' configuration" % key)
+            info_msg = "INFO: Using the '{}' configuration".format(key)
+            print(info_msg)
             config = config[key]
         else:
-            raise IOError("Configuration `%s` does not exist. Check your default file." % key)
+            err_msg = ("Configuration `{}` does not exist. "
+                       "Check your default file.").format(key)
+            raise IOError(err_msg)
+
     elif len(config) > 1:
         if 'default_config' in config:
-            print("INFO: Using default configuration '%s'" % config['default_config'])
+            info_msg = ("INFO: Using default configuration '{}'"
+                        .format(config['default_config']))
+            print(info_msg)
             config = config[config['default_config']]
         else:
-            raise IOError("You must define a 'default_config' in you configuration file.")
+            raise IOError("You must define a 'default_config' "
+                          "in you configuration file.")
     else:
         config = config[list(config)[0]]
     return config
@@ -55,11 +63,14 @@ def read_config(config, key=None):
 
 def get_config(config, configfile):
     """Get the configuration for stackyter is any."""
-    configfile = get_default_config(only_path=True) if configfile is None else configfile
+    if configfile is None:
+        configfile = get_default_config(only_path=True)
+
     if config is not None:
         # Is there a configuration file?
         if configfile is None:
-            raise IOError("No (default) configuration file found or given. Check the doc.")
+            raise IOError("No (default) configuration file found or given. "
+                          "Check the doc.")
         config = read_config(configfile, key=config)
     elif configfile is not None:
         config = read_config(configfile)
@@ -69,52 +80,87 @@ def get_config(config, configfile):
 def setup_parser():
     description = "Run Jupyter on a distant host and display it localy."
     prog = "stackyter.py"
-    usage = """%s [options]""" % prog
+    usage = "{} [options]".format(prog)
 
     parser = ArgumentParser(prog=prog, usage=usage, description=description,
                             formatter_class=ArgumentDefaultsHelpFormatter)
 
     # General options
-    parser.add_argument('-c', '--config', default=None,
-                        help='Name of the configuration to use, taken from your default '
-                        'configuration file (~/.stackyter-config.yaml or $STACKYTERCONFIG). '
-                        "Default if to use the 'default_config' defined in this file. "
-                        'The content of the configuration file will be overwritten by any '
-                        'given command line options.')
-    parser.add_argument('-f', '--configfile', default=None,
-                        help='Configuration file containing a set of option values. The content '
-                        'of this file will be overwritten by any given command line options.')
-    parser.add_argument('-H', "--host", default=None,
-                        help="Name of the target host. Allows you to connect to any host "
-                        "on which Jupyter is available, or to avoid conflit with the "
-                        "content of your $HOME/.ssh/config.")
-    parser.add_argument('-u', '--username',
-                        help="Your user name on the host. If not given, ssh will try to "
-                        "figure it out from you ~/.ssh/config or will use your local user name.")
-    parser.add_argument('-w', "--workdir", default=None,
-                        help="Your working directory on the host")
-    parser.add_argument('-j', "--jupyter", default="notebook",
-                        help="Either launch a jupiter notebook or a jupyter lab.")
-    parser.add_argument("--mysetup", default=None,
-                        help="Path to a setup file (on the host) that will be used to set up the "
-                        "working environment. A Python installation with Jupyter must be "
-                        "available to make this work.")
-    parser.add_argument("--runbefore", default=None,
-                        help="A list of extra commands to run BEFORE sourcing your setup file."
-                        " Coma separated for more than one commands, or a list in the config file.")
-    parser.add_argument("--runafter", default=None,
-                        help="A list of extra commands to run AFTER sourcing your setup file."
-                        " Coma separated for more than one commands, or a list in the config file.")
-    parser.add_argument('-C', '--compression', action='store_true', default=False,
-                        help='Activate ssh compression option (-C).')
-    parser.add_argument('-S', '--showconfig', action='store_true', default=False,
-                        help='Show all available configurations from your default file and exit.')
-    parser.add_argument('-T', '--tensorboard', action='store_true', default=False,
-                        help="Lauch an instance of TensorBoard. A Python installation with "
-                        "TensorFlow must be available to make this work.")
-    parser.add_argument('-l', '--logdir', default=None,
-                        help="Absolute path to the TensorBoard log directory. This is only "
-                        "used if the TensorBoard option is selected.")
+    parser.add_argument(
+        '-c', '--config',
+        default=None,
+        help="Name of the configuration to use, taken from your default "
+             "configuration file ($HOME/.stackyter-config.yaml or "
+             "$STACKYTERCONFIG). "
+             "Default if to use the 'default_config' defined in this file. "
+             "The content of the configuration file will be overwritten by "
+             "any given command line options.")
+    parser.add_argument(
+        '-f', '--configfile',
+        default=None,
+        help="Configuration file containing a set of option values. "
+             "The content of this file will be overwritten by any given "
+             "command line options.")
+    parser.add_argument(
+        '-H', "--host",
+        default=None,
+        help="Name of the target host. Allows you to connect to any host "
+             "on which Jupyter is available, or to avoid conflict with the "
+             "content of your $HOME/.ssh/config.")
+    parser.add_argument(
+        '-u', '--username',
+        help="Your username on the host. "
+             "If not given, ssh will try to figure it out from your "
+             "~/.ssh/config or will use your local username.")
+    parser.add_argument(
+        '-w', "--workdir",
+        default=None,
+        help="Your working directory on the host.")
+    parser.add_argument(
+        '-j', "--jupyter",
+        default="notebook",
+        choices=['notebook', 'lab'],
+        help="Either launch a jupyter notebook or a jupyter lab.")
+    parser.add_argument(
+        "--mysetup",
+        default=None,
+        help="Path to a setup file (on the host) that will be used to set up "
+             "the working environment. A Python installation with Jupyter "
+             "must be available to make this work.")
+    parser.add_argument(
+        "--runbefore",
+        default=None,
+        help="A list of extra commands to run BEFORE sourcing your setup file."
+             " Coma separated for more than one commands, or a list in the "
+             "config file.")
+    parser.add_argument(
+        "--runafter",
+        default=None,
+        help="A list of extra commands to run AFTER sourcing your setup file."
+             " Coma separated for more than one commands, or a list in the "
+             "config file.")
+    parser.add_argument(
+        '-C', '--compression',
+        action='store_true',
+        default=False,
+        help='Activate ssh compression option (-C).')
+    parser.add_argument(
+        '-S', '--showconfig',
+        action='store_true',
+        default=False,
+        help="Show all available configurations from your default file "
+             "and exit.")
+    parser.add_argument(
+        '-T', '--tensorboard',
+        action='store_true',
+        default=False,
+        help="Lauch an instance of TensorBoard. A Python installation with "
+             "TensorFlow must be available to make this work.")
+    parser.add_argument(
+        '-l', '--logdir',
+        default=None,
+        help="Absolute path to the TensorBoard log directory. "
+             "This is only used if the TensorBoard option is selected.")
 
     return parser
 
@@ -129,7 +175,8 @@ def main():
         config = get_default_config(only_path=True)
         if config is not None:
             config = open(config, 'r')
-            print("Your default configuration file contains the following configuration(s).")
+            print("Your default configuration file contains the following "
+                  "configuration(s).")
             print(config.read())
             config.close()
         else:
@@ -142,7 +189,7 @@ def main():
         for opt, val in args._get_kwargs():
             # only keep option value from the config file
             # if the user has not set it up from command line
-            if opt in config and args.__dict__[opt] == default_args.__dict__[opt]:
+            if opt in config and args.__dict__[opt] == default_args.__dict__[opt]:  # noqa
                 setattr(args, opt, config[opt])
 
     # Do we have a valide host name
@@ -155,7 +202,8 @@ def main():
 
     if args.tensorboard:
         if args.logdir is None:
-            raise ValueError("You must provide a logdir path to TensorBoard (--logdir)")
+            raise ValueError("You must provide a logdir path to TensorBoard "
+                             "(--logdir)")
 
     # Make sure that we have a list (even empty) for extra commands to run
     if args.runbefore is not None:
@@ -166,8 +214,8 @@ def main():
         args.runafter = [run.replace("$", "\$")
                          for run in string_to_list(args.runafter)]
 
-    # A random port number is selected between 1025 and 65635 (included) for server side to
-    # prevent from conflict between users.
+    # A random port number is selected between 1025 and 65635 (included)
+    # for server side to prevent from conflict between users.
     port = np.random.randint(1025, high=65634)
     port_tensorboard = port + 1
 
@@ -224,7 +272,7 @@ def main():
     # Get the token number and print out the right web page to open
     cmd_list.append("export servers=\`jupyter notebook list\`")
 
-    # If might have to wait a little bit until the server is actually running...
+    # If might have to wait a little bit until the server is actually running.
     wait_cmd = ("while [[ \$servers != *'127.0.0.1:{port}'* ]];"
                 "do sleep 1;"
                 "servers=\`jupyter notebook list\`;"
